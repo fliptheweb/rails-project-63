6e4a7faa264380ffb5f07ec3e073b6f71eec2a33
Artur Kornakov <itisshortemailaddress@gmail.com>
2024-05-11 12:19:19 +0300

    feat(): add simple inputs logic

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -5,31 +5,6 @@ require_relative "hexlet_code/version"
 module HexletCode
   class Error < StandardError; end
 
-  ELEMENTS = {
-    default: {
-      tag: :input,
-      value_position: :attribute,
-      attributes: {
-        type: "text"
-      }
-    },
-    text: {
-      tag: :textarea,
-      value_position: :inside,
-      attributes: {
-        cols: 20,
-        rows: 40
-      }
-    },
-    checkbox: {
-      tag: :input,
-      value_position: :attribute,
-      attributes: {
-        type: :checkbox
-      }
-    }
-  }
-
   # What is wrong here?
   # autoload(:Tag, "./lib/hexlet_code/tag.rb")
 
@@ -44,30 +19,65 @@ module HexletCode
     end
   end
 
-  # TODO: formatting
-  def self.form_for(data = {}, url = "#")
-    @fields_data = data
-    attributes = { action: url, method: "post" }
-    yield(self) if block_given?
-    Tag.build("form", attributes) { @all if block_given? }
+  class FormContent
+    ELEMENTS = {
+      default: {
+        tag: :input,
+        value_position: :attribute,
+        attributes: {
+          type: "text"
+        }
+      },
+      text: {
+        tag: :textarea,
+        value_position: :inside,
+        attributes: {
+          cols: 20,
+          rows: 40
+        }
+      },
+      checkbox: {
+        tag: :input,
+        value_position: :attribute,
+        attributes: {
+          type: :checkbox
+        }
+      }
+    }
+
+    def initialize(fields_data)
+      @fields_data = fields_data
+      @all = ""
+    end
+
+    def input(field_name, attributes = {})
+      as = attributes.fetch(:as, :default)
+      attributes.delete(:as)
+      element = ELEMENTS[as]
+      # Why do we need to use public_send here but not just get the value?
+      value = @fields_data.public_send(field_name)
+      tag = element[:tag]
+      attrs = element[:attributes].merge(attributes).merge({ name: field_name.to_s })
+      attrs = attrs.merge({ value: }) if element[:value_position] == :attribute
+
+      # FIXME: how to optionaly pass block to function?
+      @all += if element[:value_position] == :inside
+                Tag.build(tag, attrs) { value }
+              else
+                Tag.build(tag, attrs)
+              end
+    end
+
+    def to_s
+      @all
+    end
   end
 
-  def self.input(field_name, attributes = {})
-    @all ||= ""
-    as = attributes.fetch(:as, :default)
-    attributes.delete(:as)
-    element = ELEMENTS[as]
-    # Why do we need to use public_send here but not just get the value?
-    value = @fields_data.public_send(field_name)
-    tag = element[:tag]
-    attrs = element[:attributes].merge(attributes).merge({ name: field_name.to_s })
-    attrs = attrs.merge({ value: }) if element[:value_position] == :attribute
-
-    # FIXME: how to optionaly pass block?
-    @all += if element[:value_position] == :inside
-        Tag.build(tag, attrs) { value }
-      else
-        Tag.build(tag, attrs)
-      end
+  # TODO: formatting
+  def self.form_for(fields_data = {}, url = "#")
+    attributes = { action: url, method: "post" }
+    content = FormContent.new(fields_data)
+    yield(content) if block_given?
+    Tag.build("form", attributes) { content if block_given? }
   end
 end
